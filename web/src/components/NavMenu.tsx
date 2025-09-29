f1c4ce48cbeebca720f9746c7bff29a660795b4a
williamstyronejr <williamsjtyrone@gmail.com>
2025-09-29 03:23:42 -0400

    Initial commit

---
+++ web/src/components/NavMenu.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import MenuIcon from "./icons/MenuIcon";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarProvider,
+  SidebarTrigger,
+  useSidebar,
+  SidebarMenu,
+} from "./ui/sidebar";
+
+export default function NavMenu({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="">
+      <nav className="hidden md:flex items-center justify-between gap-4">
+        {children}
+      </nav>
+
+      <button
+        className="block md:hidden relative cursor-pointer size-6 z-20"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <MenuIcon className="size-6" />
+      </button>
+
+      <div
+        className={`bg-white h-screen px-8 pt-20 pb-6 z-10 absolute top-0 right-0 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ web/src/components/icons/MenuIcon.tsx
@@ -0,0 +1,20 @@
+export default function MenuIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M3 12H21M3 6H21M3 18H21"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
