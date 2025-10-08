c158cb228823926c44ddbf1d0b39c0a8dd5f7cfa
Tyrone Williams <williamsjtyrone@gmail.com>
2025-10-08 02:58:07 -0400

    Exporting delete user function

--- web/src/lib/auth-client.ts
+++ web/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "./auth";
 
-export const { signIn, signUp, useSession, signOut, $ERROR_CODES } =
+export const { signIn, signUp, useSession, signOut, $ERROR_CODES, deleteUser } =
   createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     plugins: [inferAdditionalFields<typeof auth>()],
