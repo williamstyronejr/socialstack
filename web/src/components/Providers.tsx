9abb3bdc15fe0d0b1facbb9b53bc1d5ad44df825
Tyrone Williams <williamsjtyrone@gmail.com>
2025-11-06 23:09:30 -0500

    React query

--- web/src/components/Providers.tsx
+++ web/src/components/Providers.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
+
+let browserClient: QueryClient | undefined = undefined;
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+      },
+    },
+  });
+}
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserClient) browserClient = makeQueryClient();
+    return browserClient;
+  }
+}
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
