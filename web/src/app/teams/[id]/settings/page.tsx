dddc955e9524a6a1ff4afff2256feb2507f39964
Tyrone Williams <williamsjtyrone@gmail.com>
2025-11-18 06:08:58 -0500

    Add new settings options

--- web/src/app/teams/[id]/settings/page.tsx
+++ web/src/app/teams/[id]/settings/page.tsx
@@ -1,4 +1,21 @@
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import query from "@/lib/query";
+import { getSessionUser } from "@/lib/auth";
+import UpdateTeamForm from "./UpdateTeamForm";
 import DeleteForms from "./DeleteForms";
+import TransferForm from "./TransferForm";
+
+async function getData(teamId: string) {
+  const user = await getSessionUser(await headers());
+
+  const team = await query.teams.getTeamById(teamId);
+
+  if (!team) return redirect("/dashboard");
+  return {
+    team,
+  };
+}
 
 export default async function TeamSettingsPage({
   params,
@@ -6,6 +23,7 @@ export default async function TeamSettingsPage({
   params: Promise<{ id: string }>;
 }) {
   const { id: teamId } = await params;
+  const { team } = await getData(teamId);
 
   return (
     <section>
@@ -14,17 +32,12 @@ export default async function TeamSettingsPage({
       </header>
 
       <div className="bg-white p-4 rounded-md mx-6">
-        <div>
-          <h2 className="text-2xl font-bold">Team</h2>
-        </div>
+        <UpdateTeamForm teamId={teamId} team={team} />
 
         <div>
           <h2 className="text-2xl font-bold">Team</h2>
-        </div>
 
-        <div>
-          <h2 className="text-2xl font-bold"></h2>
-
+          <TransferForm teamId={teamId} />
           <DeleteForms teamId={teamId} />
         </div>
       </div>
